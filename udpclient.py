0cd76c934ede03e8bba0b0e0509591552cf7f46b
cranewe <zs040311@qq.com>
2024-06-07 17:08:20 +0800

    udp_code

---
+++ udpclient.py
@@ -0,0 +1,133 @@
+import socket
+import time
+import argparse
+import struct
+import random
+import statistics
+
+#接收命令行输入
+parser = argparse.ArgumentParser(description="UDP Client")
+parser.add_argument("server_ip", type=str, help="Server IP address")
+parser.add_argument("server_port", type=int, help="Server port number")
+args = parser.parse_args()
+
+SERVER_IP = args.server_ip
+SERVER_PORT = args.server_port
+BUFFER_SIZE = 2048
+TIMEOUT = 0.1  # 100ms
+MAX_RETRIES = 2
+VERSION = 2
+STUDENT_ID = "221002606张宸玮"
+BIRTHDAY = "04-03-11"
+
+received_packets=0
+total_packets=0
+RTTS=[]#存放所有RTT
+first_server_time=None#记录第一次response的系统时间
+last_server_time=None#记录最后一次response的系统时间
+
+# 创建UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.settimeout(TIMEOUT)
+
+server_address = (SERVER_IP, SERVER_PORT)
+
+def get_current_time():
+    return time.strftime('%H-%M-%S')
+
+def create_packet(seq_no, ver, student_id, birthday_time,attempts): 
+    # client->server报文格式规定为2字节序号，1字节版本号，18字节学号，8字节本人的生日，1字节当前序号的报文的已发送次数，剩下所有存放随机数字（共计203字节）
+    # 序号和版本号
+    seq_no_bytes = struct.pack('!H', seq_no)
+    ver_bytes = struct.pack('!B', ver)
+
+    # 学号（18 bytes）
+    student_id_bytes = student_id.encode('utf-8')
+    student_id_bytes = student_id_bytes[:18]
+    student_id_bytes = student_id_bytes.ljust(18, b'\x00') 
+
+    # 生日（8 bytes）
+    birthday_bytes = birthday_time.encode('utf-8')
+    birthday_bytes = birthday_bytes[:8]
+    birthday_bytes = birthday_bytes.ljust(8, b'\x00')
+
+    #当前序号的报文的已发送次数（1 bytes）
+    send_attempts=struct.pack('!B',attempts)
+
+    # 剩余均为随机数字
+    random_bytes = ''.join(random.choices('0123456789', k=173)).encode('utf-8')
+
+    # 拼接所有部分
+    packet = seq_no_bytes + ver_bytes + student_id_bytes + birthday_bytes + send_attempts+random_bytes
+    return packet
+
+def parse_packet(packet):#解析报文信息
+    seq_no = struct.unpack('!H', packet[:2])[0]
+    ver = struct.unpack('!B', packet[2:3])[0]
+    student_id = packet[3:21].decode('utf-8').strip('\x00')
+    system_time = packet[21:29].decode('utf-8').strip('\x00')
+    random_bytes = packet[29:].decode('utf-8')
+    return seq_no, ver, student_id, system_time, random_bytes
+
+
+sock.sendto("SYN".encode(), server_address)#发送SYN给server
+data, server = sock.recvfrom(BUFFER_SIZE)
+if data.decode() == "SYN-ACK":#接收SYN-ACK
+  sock.sendto("ACK".encode(), server_address)
+  print("成功与服务器建立连接！\n")
+# 成功建立连接，客户端向服务器连续发送12个client报文
+  for i in range(1, 13):
+    attempts = 1
+    while attempts <= MAX_RETRIES+1:#当重传不超过两次时
+        current_time = get_current_time()
+        message = create_packet(i, VERSION, STUDENT_ID,BIRTHDAY,attempts)
+        print(f"Sending: Seq_no={i}, Attempt: {attempts}, Time: {current_time}")
+        start_time = time.perf_counter()
+        sock.sendto(message, server_address)
+        
+        try:
+            data, server = sock.recvfrom(BUFFER_SIZE)
+            end_time = time.perf_counter()
+            rtt = (end_time - start_time) * 1000  # 计算RTT
+            seq_no, ver, student_id, server_time, random_data = parse_packet(data)
+            print(f"Sequence_no={seq_no}, serverIP:{SERVER_IP},serverPort:{SERVER_PORT}, RTT: {rtt:.2f} ms, Student_ID: {student_id}, Server Time: {server_time}")#输出response报文信息
+            received_packets+=1
+            total_packets+=1
+            RTTS.append(rtt)
+            if first_server_time==None:
+                first_server_time=end_time
+            last_server_time=end_time
+            break
+        except socket.timeout:#发生丢包，重传
+            print(f"Sequence_no: {i},Request {i} timed out on attempt {attempts}")
+            total_packets+=1
+            attempts += 1
+
+        if attempts > MAX_RETRIES+1:#重传两次失败，停止重传
+            print(f"Request {i} failed after {MAX_RETRIES} retries, giving up")
+  sock.close()
+#计算丢包率，RTT最大值，最小值，平均值，方差，总系统响应时间
+  drop_rate=(1-received_packets/total_packets)*100
+  if RTTS:
+    max_rtt=max(RTTS)
+    min_rtt=min(RTTS)
+    ave_rtt=sum(RTTS)/len(RTTS)
+    if len(RTTS)>1:
+        s_rtt=statistics.stdev(RTTS)
+    else:
+        s_rtt=0
+  else:
+     max_rtt = min_rtt = ave_rtt = s_rtt = 0
+ 
+  total_time=last_server_time-first_server_time
+
+#针对发送的12个报文输出汇总信息
+  print(f"\n【汇总信息】")
+  print(f"1. 接收到的 UDP packets 数目: {received_packets}")
+  print(f"2. 丢包率: {drop_rate:.2f}%")
+  print(f"3. 最大 RTT: {max_rtt:.2f} ms, 最小 RTT: {min_rtt:.2f} ms, 平均 RTT: {ave_rtt:.2f} ms, RTT 标准差: {s_rtt:.2f} ms")
+  print(f"4. Server 的整体响应时间: {total_time:.4f} 秒")
+
+else:
+    print("建立连接失败！")
+    sock.close()
---
+++ udpserver.py
@@ -0,0 +1,83 @@
+import socket
+import random
+import time
+import struct
+
+SERVER_IP = "0.0.0.0"
+SERVER_PORT = 8080
+BUFFER_SIZE = 2048
+DROP_RATE = 0.4  # 规定0.4的丢包率SS
+VERSION = 2
+STUDENT_ID = "221002606张宸玮"
+
+def get_current_time():#格式化时间
+    return time.strftime('%H-%M-%S')
+
+def parse_packet(packet):#解析报文的信息，逐一返回
+    seq_no = struct.unpack('!H', packet[:2])[0]
+    ver = struct.unpack('!B', packet[2:3])[0]
+    student_id = packet[3:21].decode('utf-8').strip('\x00')
+    birthday_time = packet[21:29].decode('utf-8').strip('\x00')
+    attempts=struct.unpack('!B',packet[29:30])[0]
+    random_bytes = packet[30:].decode('utf-8')
+    return seq_no, ver, student_id, birthday_time,attempts, random_bytes
+
+def create_packet(seq_no, ver, student_id, system_time): 
+    # server->client报文结构规定为2字节序号，1字节版本号，18字节学号，8字节系统时间，剩下所有存放随机数字（共计203字节）
+    # 序号和版本号
+    seq_no_bytes = struct.pack('!H', seq_no)
+    ver_bytes = struct.pack('!B', ver)
+
+    # 学号（18 bytes）
+    student_id_bytes = student_id.encode('utf-8')
+    student_id_bytes = student_id_bytes[:18]
+    student_id_bytes = student_id_bytes.ljust(18, b'\x00') 
+
+    # 系统时间（8 bytes）
+    system_time_bytes = system_time.encode('utf-8')
+    system_time_bytes = system_time_bytes[:8]
+    system_time_bytes = system_time_bytes.ljust(8, b'\x00')
+
+    # 剩余随机数字
+    random_bytes = ''.join(random.choices('0123456789', k=174)).encode('utf-8')
+
+    # 拼接所有部分
+    packet = seq_no_bytes + ver_bytes + student_id_bytes + system_time_bytes + random_bytes
+    return packet
+
+
+# Create UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((SERVER_IP, SERVER_PORT))
+
+print(f"Server listening on {SERVER_IP}:{SERVER_PORT}")
+
+while True:#持续等待接受客户端发送的报文
+    data, addr = sock.recvfrom(BUFFER_SIZE)
+    if data.decode() == "SYN":#接收SYN
+        sock.sendto("SYN-ACK".encode(), addr)#发送SYN-ACK
+        data, addr = sock.recvfrom(BUFFER_SIZE)
+        if data.decode() == "ACK":#接收到ACK
+              print(f"成功与客户端{addr}建立连接！\n")
+              while True:
+                    data, addr = sock.recvfrom(BUFFER_SIZE)                  
+                    seq_no, ver, student_id, birthday, attempts,random_data = parse_packet(data)
+                    print(f"Received message from {addr}: Seq_no={seq_no}, Ver={ver}, Student_ID={student_id}, Attempts={attempts}, Brthday_Time={birthday}")#输出接受到的client报文信息
+
+                    #按照丢包率随机丢失client报文
+                    if random.random() < DROP_RATE:
+                        print("Dropping packet")
+                        if attempts==3 and seq_no==12:#最后一个client报文重传失败
+                            break
+                        else:
+                            continue
+
+                    #向client发送response
+                    server_time = get_current_time()  
+                    response = create_packet(seq_no, ver, student_id, server_time)
+                    sock.sendto(response, addr)
+                    print(f"Response sent: Seq_no={seq_no}, Time={server_time}")
+                    if seq_no==12:
+                      break
+        else:
+            print(f"无法与客户端{addr}建立连接")
